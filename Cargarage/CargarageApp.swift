328c32b34eb09ac38a7760a1951dddfe70297b0e
RAM6IOS <ios191169@gmail.com>
2023-08-02 22:52:54 +0100

    Initial Commit

---
+++ Cargarage/CargarageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CargarageApp.swift
+//  Cargarage
+//
+//  Created by Ramzi Bouchedoub on 2/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct CargarageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
