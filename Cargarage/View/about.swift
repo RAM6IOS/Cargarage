f7f85b65bc7797f54af761f89c3618486d19ae09
RAM6IOS <ios191169@gmail.com>
2023-09-20 14:05:33 -0700

    add : abut

---
+++ Cargarage/View/about.swift
@@ -0,0 +1,25 @@
+//
+//  about.swift
+//  Cargarage
+//
+//  Created by MACBOOK PRO on 20/9/2023.
+//
+
+import SwiftUI
+
+struct about: View {
+    var body: some View {
+        VStack{
+            Text("""
+ application provides car repair services in many cities around Saudi Arabia, including Riyadh, Khobar, Damam, Dhahran, Jubail, Qassim, Jeddah, Makkah, and Madinah. Carefer's main goal is to assist drivers with fixable and easy repair solutions
+ 
+ """)
+        }
+    }
+}
+
+struct about_Previews: PreviewProvider {
+    static var previews: some View {
+        about()
+    }
+}
