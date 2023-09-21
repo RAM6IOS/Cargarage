258bc0f84c4533bde6c935adc11b8527b92f9846
RAM6IOS <ios191169@gmail.com>
2023-09-21 14:24:57 -0700

    add: SendEmail view Model

---
+++ Cargarage/ViewModel/SendEmail.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Cargarage
+//
+//  Created by MACBOOK PRO on 21/9/2023.
+//
+
+import Foundation
+
+class SendEmailVM : ObservableObject {
+    @Published var isPresenting = false
+    var name = "name"
+    var name2 = "name"
+    var name3 = "name"
+   
+}
